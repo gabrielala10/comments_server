29f8ccd281f5d98fc2e3cb5fac3ce1e4db887628
gabrielala10 <gabriela_m18@outlook.com>
2025-03-23 13:33:10 -0300

    cicd: update unit tests

--- app/test_main.py
+++ app/test_main.py
@@ -2,12 +2,21 @@ import unittest
 import main 
 
 class TestMain(unittest.TestCase):
+  @classmethod
+  def setUpClass(test):
+    test.app = main.app.test_client()
+    test.app.testing = True  
   def test_save_comment(self):
-    comment = {"email": "test_unit@test.com", "comment": "Test unit in process!", "content_id": 10}
-    main.api_comment_new(comment)
+    request_data = {
+        "email": "test_unit@test.com",
+        "comment": "Test unit test!",
+        "content_id": 10
+    }
+    self.app.post('/api/comment/new', json=request_data)
     comments, status = main.api_comment_list(10)
     self.assertEqual(status, 200)
     self.assertEqual(len(comments), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
