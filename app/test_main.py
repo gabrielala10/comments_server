e405f7039c9b2bcf1454533e26b61e571af8c9b3
gabrielala10 <gabriela_m18@outlook.com>
2025-03-23 13:42:35 -0300

    cicd: update unit tests to http requests

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,30 @@
 import unittest
-import main 
+import main
+from flask import json
 
 class TestMain(unittest.TestCase):
   @classmethod
   def setUpClass(test):
     test.app = main.app.test_client()
     test.app.testing = True  
+
   def test_save_comment(self):
     request_data = {
         "email": "test_unit@test.com",
         "comment": "Test unit test!",
         "content_id": 10
     }
-    self.app.post('/api/comment/new', json=request_data)
-    comments, status = main.api_comment_list(10)
-    self.assertEqual(status, 200)
-    self.assertEqual(len(comments), 1)
 
+    response = self.app.post('/api/comment/new', json=request_data)
+    response_data = json.loads(response.data)
+    self.assertEqual(response_data['status'], 'SUCCESS')
+    
+    response = self.app.get('/api/comment/list/10')
+    self.assertEqual(response.status_code, 200)
+    comments = json.loads(response.data)
+    self.assertEqual(len(comments), 1)
+    self.assertEqual(comments[0]['email'], "test_unit@test.com")
+    self.assertEqual(comments[0]['comment'], "Test unit test!")
 
 if __name__ == '__main__':
     unittest.main()
