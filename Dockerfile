064514b3d90ac89454112738111f1b445b8e662d
gabrielala10 <gabriela_m18@outlook.com>
2025-03-22 23:04:58 -0300

    app: update unit tests and containers to the new api

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 # Dockerfile
-FROM python:3.11-slim
+FROM python:3.7.4-slim
 
 WORKDIR /app
 
 COPY app/. .
 RUN pip install -r requirements.txt
 
-EXPOSE 5000
+EXPOSE 8000
 
 CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "main:app"]
--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,11 @@
 import unittest
-from main import load_comments, save_comment
+import main 
 
 class TestMain(unittest.TestCase):
   def test_save_comment(self):
     comment = {"email": "test_unit@test.com", "comment": "Test unit in process!", "content_id": 10}
-    save_comment(comment)
-    comments, status = load_comments(10)
+    main.api_comment_new(comment)
+    comments, status = main.api_comment_list(10)
     self.assertEqual(status, 200)
     self.assertEqual(len(comments), 1)
 
