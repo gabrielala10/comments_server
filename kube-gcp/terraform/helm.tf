4e380a4f06b9862af8c4bf04637779781a00318e
gabrielala10 <gabriela_m18@outlook.com>
2025-03-25 21:24:45 -0300

    iac: add helm via terraform

---
+++ kube-gcp/terraform/helm.tf
@@ -0,0 +1,37 @@
+# Instala o Helm Provider
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+# Instala o Helm no cluster
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = "monitoring"
+  create_namespace = true
+
+  set {
+    name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
+    value = "false"
+  }
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = "monitoring"
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "admin"
+    value = "admin"
+  }
+}
